89bf73300b47eea7e5aef022331534f1282613df
Fredin <90355410+fredinj@users.noreply.github.com>
2024-10-18 21:47:34 +0530

    cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes.main import main_routes
 from .routes.api import api
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object('config.Config')
 
     # Register the blueprint
