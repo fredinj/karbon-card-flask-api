c33f59eeab1d300de921f75552c52d2c5a2dc439
Fredin <90355410+fredinj@users.noreply.github.com>
2024-10-18 21:40:21 +0530

    add port binding

--- run.py
+++ run.py
@@ -4,4 +4,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # Running the Flask app with debug mode enabled
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0',port=5000)
+    
